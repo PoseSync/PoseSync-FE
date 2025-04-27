d1353ac18b6358eb60c319f2931f3b5da90d291e
mrangjw <157506327+mrangjw@users.noreply.github.com>
2025-04-27 18:01:49 +0900

    초기 프로젝트 설정: React + Vite + TypeScript + styled-components 구성

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Title } from "../../styles/pages/home/index.style";
+
+const HomePage: React.FC = () => {
+  return (
+    <Container>
+      <Title>PoseSync 프로젝트 시작</Title>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ src/styles/pages/home/index.style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f3f4f6;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.875rem;
+  font-weight: 700;
+  color: #2563eb;
+  margin-bottom: 1rem;
+`;
